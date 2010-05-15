86194cb14660ceb43fcdf8a76233c075644cf7f8
Bozhidar Batsov <lordbad@gmail.com>
2010-05-15 14:25:22 +0300

    - added thesis skeleton

---
+++ auto/master.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-run-style-hooks
+     "fontenc"
+     "T2A"
+     "babel"
+     "english"
+     "bulgarian"
+     "inputenc"
+     "utf8"
+     "listings"
+     "latex2e"
+     "rep10"
+     "report"
+     "title"
+     "intro"
+     "overview"
+     "modules"
+     "implementation"
+     "manual"
+     "conclusion")))
+
